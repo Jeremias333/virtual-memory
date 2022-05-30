7d2ad72e13e3f5b0a846c7de2c60bd1b35d48ae3
jeremias <jeremiasprogrammer@gmail.com>
2022-05-30 13:20:46 -0300

    add exceptions to start program

--- src/main.c
+++ src/main.c
@@ -2,8 +2,33 @@
 #include "string.h"
 #include "stdlib.h"
 
-int main(){
-    printf("Dev area operating...");
+int choice_path(char **argv);
+
+int main(int argc, char **argv) {
+    char address_path = argv[0];
+    printf("Dev area operating... %s", address_path);
+
     return 1;
 }
 
+int choice_path(char **argv){
+    int path = 0;
+    /*
+        0 fifo and fifo
+        1 fifo and lru
+        2 lru and fifo
+    */
+
+    if(argv[2] == NULL && argv[3] == NULL){
+        path = 0;
+        // Os dois valores passados são nulos, nesse caso usaremos fifo para paginação e TLB. 
+    }else if (argv[2] == "fifo" && argv[3] == "fifo"){
+        path = 0;
+    }else if(argv[2] == "fifo" && argv[3] == "lru"){
+        path = 1;
+    }else if(argv[2] == "lru" && argv[3] == "fifo"){
+        path = 2;
+    }
+
+    return path;
+}
