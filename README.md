edb1a2fc4f7c31a65eb1e4fb0229cf0dfffb5e24
jeremias <jeremiasprogrammer@gmail.com>
2022-06-24 17:20:44 -0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # virtual-memory
+
+### Este é um programa que simula uma memória virtual.
+
+## Para utiliza-lo é necessário ter uma máquina UNIX (linux ou mac) e os seguintes arquivos:
+
+### - Arquivo com os endereços virtuais: address.txt
+### - Arquivo que simula a memória secundária (BACKING_STORE): BACKING STORE.bin
+
+
+## Como executar:
+
+### - Existem 3 parametros extremamente necessários para execução. O primeiro é o arquivo de endereços virtuais (nome.extenção), o segundo é o algoritmo de substituição da page table e o terceiro algoritmo de substituição da TLB, podendo variar entre lru e fifo.
+
+### - Exemplo: ./vm address.txt fifo fifo
+
+### - Utilizar o seguinte comando para limpar o arquivo executável gerado: make clean
+
+### - Utilizar o seguinte comando para gerar um novo arquivo executável: make
+
+### - A execução dele não funciona no make por conta dos parametros que devem ser passados, utilize o exemplo mostrado.
+
--- dev/main.c
+++ dev/main.c
@@ -12,18 +12,13 @@
 #define CLOSE "PROGRAMA ENCERRADO\n"
 
 //Page CONSTANTES
-#define PAGE_SIZE 128
-#define PAGE_ENTRIES 128
-
-//Frame CONSTANTES
-#define FRAME_SIZE 128
-#define FRAME_ENTRIES 128
+#define PAGE_SIZE 256
 
 //tlb CONSTANTES
 #define TLB_ENTRIES 16
 
 //Memory CONSTANTES
-#define MEMORY_SIZE (FRAME_SIZE * FRAME_ENTRIES)
+#define MEMORY_SIZE 128
 
 //BACKING_STORE CONSTANTES
 #define BACKING_STORE_FILE_NAME "BACKING_STORE.bin"
@@ -34,7 +29,12 @@
 FILE *arq_address;
 FILE *arq_correct_output;
 
-char physical_memory[MEMORY_SIZE];
+typedef struct {
+   char physical_memory[MEMORY_SIZE];
+} physicalMemory;
+
+physicalMemory physical_memory_array[MEMORY_SIZE];
+
 
 int virtual_address;
 int physical_address;
@@ -44,15 +44,20 @@ int frame_number;
 int value;
 int memory_index = 0;
 
-
 int page_and_tlb_path = 0;
 
-int backing_store_file_descriptor;
+FILE* backing_store_file_descriptor;
 char* backing_store_data;
 
 int page_fault_counter = 0;
 
-int page_table[PAGE_ENTRIES];
+typedef struct {
+    int frame;
+    int bit;
+    int time;
+} pageTableStruct;
+
+pageTableStruct page_table[PAGE_SIZE];
 
 
 //function MODELS DECLARATIONS
@@ -104,11 +109,7 @@ int main(int argc, char **argv) {
 
     initialize_page_table();
 
-    read_backing_store();
-
     read_and_process_address(address_path);
-
-    close(backing_store_file_descriptor);
     return 0;
 }
 
@@ -125,7 +126,7 @@ int choice_path(char **argv){
     //Caso o segundo parametro não existe, o 3º parametro é impossivel de existir
     if(argv[2] == NULL){
         path = -2;
-
+ 
     // Os dois valores passados são nulos.
     }else if(argv[2] != NULL && argv[3] == NULL){
         path = -1;
@@ -157,7 +158,6 @@ void print_err(char *err){
 void read_and_process_address(char *file_name){
     arq_address = fopen(file_name, "r");
     char line[7];
-    char *array[7];
 
     if (arq_address == NULL){
         char *ERR = DEFAULT_ERR_MSG"Ao abrir o arquivo. Está vazio ou não existe";
@@ -169,69 +169,46 @@ void read_and_process_address(char *file_name){
         fgets(line, sizeof(line), arq_address);  // o 'fgets' lê até 7 caracteres ou até o '\n'
         virtual_address = atoi(line);
 
-        // printf("%d\n", virtual_address);
-        
         page_number = get_page_number(virtual_address);
 
+        // fprintf(arq_correct_output, "PAGE NUMBER: %d\n", page_number);
+
         offset = get_offset(virtual_address);
+
+        // fprintf(arq_correct_output, "OFFSET: %d\n", offset);
 
         // Implementar aqui a chamada da TLB
 
         // Caso não seja encontrado na TLB, busca na page table
+
         // Pegando número de frame na page table
-        frame_number = get_frame_number(page_number);
+        int frame_number_value = get_frame_number(page_number);
 
-        if (frame_number == -1){
-            physical_address = frame_number + offset;
+        if (frame_number_value == -1){
+            //fazendo leitura do backing store e escrevendo valor referente ao page_number ao
+            //meu array de char physical_memory
+            read_backing_store(page_number);
 
-            // Implementar aqui a chamada da atualização da TLB
-
-            value = physical_memory[physical_address];
-
-        }else{
-            
-            // Na condição acima deu page fault então será necessário buscar no backing store
-            // para então armazenar num local disponível na memória física.
-
-            int page_address = page_number * PAGE_SIZE;
-
-            // Fazendo verificação do frame livre existente
-            if(memory_index != -1){
-                // existe um frame livre
-                // Faremos então o armazenamento do valor no backing store
-                memcpy(physical_memory + memory_index, backing_store_data + page_address, PAGE_SIZE);
-
-                frame_number = memory_index;
-
-                physical_address = frame_number + offset;
-
-                value = physical_memory[physical_address];
-                printf("Dentro de uma memória livre");
-                fprintf(arq_correct_output, "Dentro de uma memória livre\n");
-
-                page_table[page_number] = memory_index;
-
-                // Implementar chamada para atualização da TLB
-
-                // Incrementar memory index
-                if(memory_index < MEMORY_SIZE - FRAME_SIZE){
-                    memory_index += FRAME_SIZE;
-                }else{
-                    // Em caso da memória cheia essa variável estará marcada com -1
-                    memory_index = -1;
-                }
-
-            }else{
-                // Não existe um frame livre na memória física
-                printf("Não existe memória livre\n");
-                fprintf(arq_correct_output, "Não existe memória livre\n");
+            if(memory_index == 127){
+                memory_index = 0;
             }
 
+            frame_number = memory_index;
+            physical_address = (frame_number * 256) + offset;
+            memory_index++;
+            // Implementar aqui a chamada da atualização da TLB
 
+            page_table[page_number].frame = frame_number;
+            page_table[page_number].bit = 1;
+
+            value = physical_memory_array[frame_number].physical_memory[offset];
+
+        }else{
+            physical_address = (frame_number_value * 256) + offset;
+            printf("%d\n", frame_number_value);
+            value = physical_memory_array[frame_number_value].physical_memory[offset];
         }
 
-        // printf("Endereço Virtual: %d Endereço Físico: %d Valor: %d\n", virtual_address, physical_address, value);
-        // *arq_correct_output.write("Virtual Address: %d Physical Address: %d Value: %d\n", virtual_address, physical_address, value);
         fprintf(arq_correct_output, "Virtual Address: %d Physical Address: %d Value: %d\n", virtual_address, physical_address, value);
     }
 
@@ -239,26 +216,24 @@ void read_and_process_address(char *file_name){
     fclose(arq_correct_output);
 }
 
-void read_backing_store(){
+void read_backing_store(int page_number){
     //Abrindo arquivo
-    backing_store_file_descriptor = open(BACKING_STORE_FILE_NAME, O_RDONLY);
-    
-    //Fazendo mapeamento para a memoria
-    backing_store_data = mmap(0, MEMORY_SIZE, PROT_READ, MAP_SHARED, backing_store_file_descriptor, 0);
+    backing_store_file_descriptor = fopen(BACKING_STORE_FILE_NAME, "rb");
 
-    if (backing_store_data == MAP_FAILED){
-        char *ERR = DEFAULT_ERR_MSG"Ao tentar mapear o backing store";
+    if (backing_store_file_descriptor == NULL){
+        char *ERR = DEFAULT_ERR_MSG"Ao tentar ler o backing store";
         print_err(ERR);
-        close(backing_store_file_descriptor);
+        fclose(backing_store_file_descriptor);
         exit(1);
     }
-}
 
-void initialize_page_table() {
-    //Inicializando a page table com -1 para saber que não está preenchido.
-    for (int i = 0; i < PAGE_ENTRIES; i++) {
-        page_table[i] = -1;
-    }
+    //Movendo ponteiro
+    fseek(backing_store_file_descriptor, PAGE_SIZE * page_number, SEEK_SET);
+    //Lendo dados
+    fread(physical_memory_array[memory_index].physical_memory, PAGE_SIZE, 1, backing_store_file_descriptor);
+    //Fechando arquivo
+    fclose(backing_store_file_descriptor);
+    // printf("Page number: %d\n", page_number);
 }
 
 int get_page_number(int virtual_address){
@@ -272,11 +247,12 @@ int get_offset(int virtual_address){
 }
 
 int get_frame_number(int page_number){
-   if (page_table[page_number] == -1) {
+    if (page_table[page_number].frame == -1) {
         page_fault_counter++;
+        return -1;
     }
 
-    return page_table[page_number];
+    return page_table[page_number].frame;
 }
 
 void prepare_to_write_correct(){
@@ -287,4 +263,12 @@ void prepare_to_write_correct(){
 
     //Abrindo arquivo para append no arquivo
     arq_correct_output = fopen(CORRECT_FILE_NAME, "a");
+}
+
+void initialize_page_table(){
+    for (int i = 0; i < PAGE_SIZE; i++) {
+        page_table[i].frame = -1;
+        page_table[i].bit = -1;
+        page_table[i].time = -1;
+    }
 }
